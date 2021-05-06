58f8225e6399e6eb39dc5124eb094b04e4c6238f
yanhan-si <syanhan96@gmail.com>
2021-05-06 14:54:25 +0800

    add src

---
+++ src/lesson1/creating_classes/Person.java
@@ -0,0 +1,31 @@
+package lesson1.creating_classes;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+
+    public Person (String firstName, String lastName){
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    @Override
+    public String toString() {
+        return "Name:" + firstName + " " + lastName;
+    }
+}
